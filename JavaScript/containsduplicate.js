17ca7fe69c9988860c2f155755ae20ddacde672c
naveenrs123 <naveen.sivasankar1234@gmail.com>
2023-07-26 21:09:38 -0500

    contains duplicate 217

---
+++ JavaScript/containsduplicate.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+
+
+// Hash Map Approach
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let dict = {};
+    for (let num of nums) {
+        if (dict[num] >= 1) {
+            return true;
+        }
+
+        dict[num] = 1;
+    } 
+    return false;
+};
+
+// Hash Set Approach
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// Slightly better in practice because a hash set uses less memory.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set = new Set();
+    for (let num of nums) {
+        if (set.has(num)) {
+            return true;
+        }
+        set.add(num);
+    }
+    return false;
+};
+
+console.log(containsDuplicate([1, 2, 4, 4]))
+console.log(containsDuplicate([1, 2, 3]))
