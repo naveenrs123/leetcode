e0595f80b128d425ad03f680a4bf0dcfe95e68e6
naveenrs123 <naveen.sivasankar1234@gmail.com>
2023-07-29 13:59:02 -0500

    two sum and file reorganization

---
+++ JavaScript/arrays_and_hashing/twosum.js
@@ -0,0 +1,21 @@
+// Optimized Solution
+// Hash table to check the difference
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let numIndexDict = {};
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (diff in numIndexDict) {
+            return [numIndexDict[diff], i]
+        }
+
+        numIndexDict[nums[i]] = i;
+    }
+};
