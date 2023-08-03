7b3941ebc8da68464bb199298a85a95a1b9b1632
naveenrs123 <naveen.sivasankar1234@gmail.com>
2023-08-02 21:47:13 -0700

    buy and sell stock Python

---
+++ Python/sliding_window/buy_and_sell_stock.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            profit = prices[r] - prices[l]
+            if profit >= 0:
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+
+        return maxP
