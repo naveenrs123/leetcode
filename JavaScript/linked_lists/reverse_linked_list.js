26a2fd65f1c363cc96988d0d708d9a8dda199571
naveenrs123 <naveen.sivasankar1234@gmail.com>
2023-09-04 17:13:44 -0700

    Reverse linked list JS

---
+++ JavaScript/linked_lists/reverse_linked_list.js
@@ -0,0 +1,65 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// Iterative
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (head == null || head.next == null) {
+    return head;
+  }
+
+  let prev = null;
+  let curr = head;
+
+  while (curr != null) {
+    let next = curr.next;
+    curr.next = prev;
+
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// Recursive
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+    if (head === null) return head;
+
+    let newHead = head;
+    if (head.next !== null) {
+        newHead = reverseList(head.next);
+        head.next.next = head;
+    }
+
+    head.next = null;
+    return newHead;
+};
